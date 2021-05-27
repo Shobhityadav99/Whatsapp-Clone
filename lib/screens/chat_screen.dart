cc8289f1637f401a1feb93246fed86feffafeb8c
Shobhit Yadav <shobhit999000@gmail.com>
2021-05-27 12:35:19 +0530

    Chat screen done

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -1,11 +1,41 @@
 import 'package:flutter/material.dart';
+import '../models/chat_model.dart';
+class ChatScreen extends StatefulWidget {
+  @override
+  _ChatScreenState createState() => _ChatScreenState();
+}
 
-class ChatScreen extends StatelessWidget {
+class _ChatScreenState extends State<ChatScreen> {
   @override
   Widget build(BuildContext context) {
-    return new Center(
-      child: new Text("Chats",
-      style: TextStyle(fontSize: 20.0),),
+    return new ListView.builder(
+      itemBuilder: (context, index) => new Column(
+        children: <Widget>[
+          new Divider(
+            height: 10.0,
+          ),
+          new ListTile(
+            leading: new CircleAvatar(
+              foregroundColor: Theme.of(context).primaryColor,
+              backgroundColor: Colors.grey,
+              backgroundImage: new NetworkImage(DummyData[index].avatarUrl),
+            ),
+            title: new Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                new Text(DummyData[index].name,style: TextStyle(fontWeight: FontWeight.bold),),
+                new Text(DummyData[index].time,style: TextStyle(color: Colors.grey,fontSize: 14.0),)
+              ],
+            ),
+            subtitle: new Container(
+              padding: const EdgeInsets.only(top: 5.0),
+              child: Text(DummyData[index].message,
+              style: TextStyle(color: Colors.grey,fontWeight: FontWeight.bold,fontSize: 15.0),),
+            ),
+          )
+        ],
+      ),
+      itemCount: DummyData.length,
     );
   }
 }
