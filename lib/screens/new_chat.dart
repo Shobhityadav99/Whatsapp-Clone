e098f5db591b8debfd0d6a6f5d3145710a125110
Shobhit Yadav <shobhit999000@gmail.com>
2021-05-31 12:39:53 +0530

    New chat screen in progress

---
+++ lib/screens/new_chat.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+class NewChat extends StatefulWidget {
+  @override
+  _NewChatState createState() => _NewChatState();
+}
+
+class _NewChatState extends State<NewChat> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
