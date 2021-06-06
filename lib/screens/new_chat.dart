ed54b223c5190023b208f1613f6e8d001ce0c524
Shobhit Yadav <shobhit999000@gmail.com>
2021-06-06 20:48:21 +0530

    Buttons added correctly

--- lib/screens/new_chat.dart
+++ lib/screens/new_chat.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:whatsapp_clone/models/chat_model.dart';
 
 class NewChat extends StatefulWidget {
   @override
@@ -9,64 +10,94 @@ class _NewChatState extends State<NewChat> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: RichText(
-            textAlign: TextAlign.left,
-            text: TextSpan(
-                text: "Select contact",
-                style: TextStyle(fontSize: 24),
-                children: <TextSpan>[
-                  TextSpan(
-                    text: '\n420 contacts',
-                    style: TextStyle(
-                      fontSize: 14,
-                    ),
+      appBar: AppBar(
+        title: RichText(
+          textAlign: TextAlign.left,
+          text: TextSpan(
+              text: "Select contact",
+              style: TextStyle(fontSize: 24),
+              children: <TextSpan>[
+                TextSpan(
+                  text: '\n420 contacts',
+                  style: TextStyle(
+                    fontSize: 14,
                   ),
-                ]),
-          ),
-          leading: IconButton(
-              icon: Icon(
-                Icons.arrow_back,
-                color: Colors.white,
-              ),
-              onPressed: null),
-          actions: <Widget>[
-            IconButton(
-              icon: Icon(
-                Icons.search,
-                color: Colors.white,
-              ),
-              onPressed: null,
-            ),
-            VertMenu(),
-          ],
+                ),
+              ]),
         ),
-        body: Column(
-          children: <Widget>[
-            FlatButton(
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: <Widget>[
-                  Divider(
-                    height: 60,
-                  ),
-                  CircleAvatar(
-                    backgroundColor: Theme.of(context).accentColor,
-                    radius: 23,
-                  ),
-                  SizedBox(
-                    width: 15,
-                  ),
-                  Text(
-                    'New Group',
-                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
-                  ),
-                ],
-              ),
-              onPressed: null,
-            )
-          ],
-        ));
+        leading: IconButton(
+            icon: Icon(
+              Icons.arrow_back,
+              color: Colors.white,
+            ),
+            onPressed: null),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(
+              Icons.search,
+              color: Colors.white,
+            ),
+            onPressed: null,
+          ),
+          VertMenu(),
+        ],
+      ),
+      body: Column(
+        children: <Widget>[
+          FlatButton(
+            child: AddingNewChatandGroup(
+              icon: Icons.group,
+              label: 'New Group',
+            ),
+            onPressed: null,
+          ),
+          FlatButton(
+            child: AddingNewChatandGroup(
+              icon: Icons.person,
+              label: 'New Contact',
+            ),
+            onPressed: null,
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class AddingNewChatandGroup extends StatelessWidget {
+  final String label;
+  final IconData icon;
+  const AddingNewChatandGroup({
+    this.icon,
+    this.label,
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.start,
+      children: <Widget>[
+        Divider(
+          height: 60,
+        ),
+        CircleAvatar(
+          backgroundColor: Theme.of(context).accentColor,
+          child: Icon(
+            icon,
+            color: Colors.white,
+          ),
+          radius: 23,
+        ),
+        SizedBox(
+          width: 15,
+        ),
+        Text(
+          this.label,
+          style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+        ),
+      ],
+    );
   }
 }
 
